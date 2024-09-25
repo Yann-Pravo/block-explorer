512b0df37ca3edf43a3f3660790a21abe1812cd8
Yann Pravo <yann.pravo@gmail.com>
2024-09-25 09:16:12 +0200

    create 1st api call
    change api url to vercel
    
    change api url to vercel
    
    remove blocks
    
    change api url to vercel
    
    add prisma generate & deploy post install
    
    change db url from prisma schema
    
    remove get
    
    remove get

---
+++ src/app/api/block/getLastBlocks.ts
@@ -0,0 +1,24 @@
+import prisma from '@/lib/prisma'
+import { NextResponse } from 'next/server'
+
+export async function getLastBlocks() {
+  try {
+    const blocks = await prisma.block.findMany({
+      take: 10,
+      select: {
+        id: true,
+        size: true,
+        number: true,
+        timestamp: true,
+        nonce: true,
+        gasLimit: true,
+        hash: true,
+      }
+    });
+
+    return NextResponse.json({ blocks })
+  } catch {
+    return NextResponse.json({ error: 'Failed to fetch blocks' }, { status: 500 });
+  }
+}
+ 
