5200e93e1e7c084c598a9e4ada06a37de354bfe1
Yann Pravo <yann.pravo@gmail.com>
2024-09-25 19:27:02 +0200

    mv createLastBlock

---
+++ src/app/api/block/createLastBlock.ts
@@ -0,0 +1,43 @@
+import { NextResponse } from "next/server";
+import { getLastBlockFromInfura } from "../block/getLastBlockFromInfura";
+import prisma from "@/lib/prisma";
+
+export async function createLastBlock() {
+  try {
+    const response = await getLastBlockFromInfura();
+    const lastBlock = await response.json();
+
+    if (lastBlock.result) {
+      const { size, number, timestamp, nonce, gasLimit, hash } =
+        lastBlock.result;
+
+      const alreadyExists = await prisma.block.findFirst({
+        where: { hash },
+      });
+
+      if (alreadyExists) {
+        return NextResponse.json(alreadyExists);
+      } else {
+        const newBlock = await prisma.block.create({
+          data: {
+            size,
+            number,
+            timestamp,
+            nonce,
+            gasLimit,
+            hash,
+          },
+        });
+
+        return NextResponse.json(newBlock);
+      }
+    } else {
+      throw new Error();
+    }
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to create last block" },
+      { status: 500 }
+    );
+  }
+}
