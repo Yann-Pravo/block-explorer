e8f7cbe3d0f0a641f754e9c5391a82776423ca77
Yann Pravo <yann.pravo@gmail.com>
2024-09-25 15:30:09 +0200

    show blocks list

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,8 +1,42 @@
+import { Block } from "@prisma/client";
 import { getLastBlocks } from "../api/block/getLastBlocks";
 
 export default async function RoutePage() {
   const res = await getLastBlocks();
   const lastBlocks = await res.json();
 
-  return <div>nb blocks: {lastBlocks.length}</div>;
+  return (
+    <div className="overflow-hidden bg-white shadow sm:rounded-md w-2xl">
+      <ul role="list" className="divide-y divide-gray-200">
+        {lastBlocks.map((block: Block) => (
+          <li key={block.id} className="px-4 py-4 sm:px-6">
+            <div>
+              <span className="text-stone-400">Size:</span>{" "}
+              <span className="">{block.size}</span>
+            </div>
+            <div>
+              <span className="text-stone-400">Number:</span>{" "}
+              <span className="">{block.number}</span>
+            </div>
+            <div>
+              <span className="text-stone-400">Timestamp:</span>{" "}
+              <span className="">{block.timestamp}</span>
+            </div>
+            <div>
+              <span className="text-stone-400">Nonce:</span>{" "}
+              <span className="">{block.nonce}</span>
+            </div>
+            <div>
+              <span className="text-stone-400">GasLimit:</span>{" "}
+              <span className="">{block.gasLimit}</span>
+            </div>
+            <div>
+              <span className="text-stone-400">Hash:</span>{" "}
+              <span className="">{block.hash}</span>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
